512a25b615076ccaeb4c4a8b4edf9f08f4b98a50
MaJingyiUtt <jingyi.ma@student-cs.fr>
2021-03-01 19:35:12 +0100

    pdf metadata+read files content

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 """This module is the principle module of this flask application """
 
+import base64
+import os
+
+import magic
+import PyPDF2
 from flask import Flask, jsonify, request
 from PIL import Image
 from PIL.ExifTags import TAGS
-from constants import const
-import magic
-import os
 
+from constants import const
 
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = {"txt", "csv", "pdf", "jpg", "png", "gif"}
@@ -24,6 +27,9 @@ def upload_file():
             metadata = generate_metadata(filepath)
             filedata = generate_filedata(filepath)
             data = {"metadata": metadata, "filedata": filedata}
+            # remove the tmp file created
+            if os.path.exists(filepath):
+                os.remove(filepath)
             return jsonify(data)
         else:
             return const.ERROR_MESSAGE_EXTENSION_NOT_ALLOWED
@@ -35,11 +41,14 @@ def generate_metadata(filepath):
     metadata = {}
     metadata["mime"] = magic.from_file(filepath, mime=True)
     metadata["size"] = os.stat(filepath).st_size
-    if metadata["mime"].split("/")[0]=="image":
-        generate_image_metadata(filepath,metadata)
+    if metadata["mime"].split("/")[0] == "image":
+        generate_image_metadata(filepath, metadata)
+    if metadata["mime"] == "application/pdf":
+        generate_pdf_metadata(filepath, metadata)
     return metadata
 
-def generate_image_metadata(filepath,metadata):
+
+def generate_image_metadata(filepath, metadata):
     opened_image = Image.open(filepath)
     exifdata = opened_image.getexif()
     # iterating over all EXIF data fields
@@ -52,8 +61,38 @@ def generate_image_metadata(filepath,metadata):
             data = data.decode()
         metadata[tag] = data
 
+
+def generate_pdf_metadata(filepath, metadata):
+    # creating a pdf file object
+    pdfFileObj = open(filepath, "rb")
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    information = pdfReader.getDocumentInfo()
+    print(information)
+    for info in information:
+        metadata[info.split("/")[1]] = information[info]
+    pdfFileObj.close()
+
+
 def generate_filedata(filepath):
-    filedata = "aaabbbccc"
+    type = magic.from_file(filepath, mime=True)
+    filedata = ""
+    if type == "application/csv" or type == "text/plain":
+        f = open(filepath, "r")
+        filedata = f.read()
+    elif type == "application/pdf":
+        # creating a pdf file object
+        pdfFileObj = open(filepath, "rb")
+        # creating a pdf reader object
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        for page in pdfReader.pages:
+            filedata += page.extractText()
+        # closing the pdf file object
+        pdfFileObj.close()
+    elif type.split("/")[0] == "image":
+        # encode image to base64
+        image = open(filepath, "rb")
+        encoded_string = base64.b64encode(image.read())
+        filedata = encoded_string.decode("utf-8")
     return filedata
 
 
--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
-'''
+"""
 This file contains all the constants, to protext them from changing value. 
 
-'''
+"""
+
+
 class Const(object):
     class ConstError(TypeError):
         pass
@@ -10,13 +12,16 @@ class Const(object):
         pass
 
     def __setattr__(self, name, value):
-        if name in self.__dict__: # check if the contant is already defined
+        if name in self.__dict__:  # check if the contant is already defined
             raise self.ConstError("Can't change const.%s" % name)
-        if not name.isupper(): # check if the constant name is capitalized. 
+        if not name.isupper():  # check if the constant name is capitalized.
             raise self.ConstCaseError('const name "%s" is not all supercase' % name)
 
         self.__dict__[name] = value
 
+
 const = Const()
 const.ERROR_MESSAGE_NO_FILE = "Sorry, no file is uploaded. "
-const.ERROR_MESSAGE_EXTENSION_NOT_ALLOWED = "Sorry, the extension of your file is not allowed in this app."
+const.ERROR_MESSAGE_EXTENSION_NOT_ALLOWED = (
+    "Sorry, the extension of your file is not allowed in this app."
+)
